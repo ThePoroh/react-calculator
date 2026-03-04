9de1d78110da513828beb4b4e4f04458ff93e5d4
Denys Porohnavets <dporoxnavecz111@gmail.com>
2026-03-04 21:12:49 +0200

    feat: add basic calculator layout and styles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="calculator">
+      <h1>Мій Калькулятор</h1>
+      <div className="display">0</div>
+      <div className="buttons">
+        {/* Кнопки будуть тут */}
+      </div>
+    </div>
+  );
+}
+export default App;
