10da3ee64f86d8cbb95126ed6bfbe53d6858762a
Denys Porohnavets <dporoxnavecz111@gmail.com>
2026-04-01 22:27:34 +0300

    fix: exclude e2e directory from vitest

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    // 1. Ігноруємо папку e2e, щоб Vitest не "пав" через Playwright
+    exclude: ['**/node_modules/**', '**/dist/**', '**/e2e/**'],
+    // 2. Шукаємо тести тільки в папці src
+    include: ['src/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  },
 })
